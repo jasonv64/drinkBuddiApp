3b7ca2541930ed04ed3137048973da34ac00bd42
Efren Gomez <efren.gomez0630@gmail.com>
2023-06-11 19:29:41 -0700

    hamburger menu 1

--- drinkbuddi/app/map/map.js
+++ drinkbuddi/app/map/map.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, DrawerLayoutAndroid, } from 'react-native';
 import React, { useRef, useState } from 'react';
 import MapView from "react-native-maps";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 
 
-export default function Map() {
+
+export default function Map({ navigation }) {
 
     const drawer = useRef(null);
     const [drawerPosition, setDrawerPosition] = useState('left');
@@ -21,6 +23,8 @@ export default function Map() {
     const navigationView = () => (
         <View style={[styles.container, styles.navigationContainer]}>
             <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+            <Button title="Login"
+                onPress={() => navigation.navigate('Home')} />
             <Button style={styles.drawerButton}
                 title="Close drawer"
                 onPress={() => drawer.current?.closeDrawer()}
@@ -34,16 +38,20 @@ export default function Map() {
             drawerWidth={300}
             drawerPosition={drawerPosition}
             renderNavigationView={navigationView}>
-            <View style={styles.container}>
-                <Button
+            <View style={styles.container2}>
+                <FontAwesome.Button
+                    style={styles.barMenu}
+                    name="bars"
+                    borderRadius={25}
+                    onPress={() => drawer.current?.openDrawer()}>
+                </FontAwesome.Button>
+                {/* <Button
                     title="Change Drawer Position"
                     onPress={() => changeDrawerPosition()}
-                />
-                <Button
-                    style={styles.drawerButton}
-                    title="Open drawer"
-                    onPress={() => drawer.current?.openDrawer()}
-                />
+                /> */}
+            </View>
+            <View style={styles.container}>
+
                 <MapView
                     style={styles.map}
                     initialRegion={{
@@ -60,15 +68,32 @@ export default function Map() {
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1, //the container will fill the whole screen.
+        flex: 5, //the container will fill the whole screen.
         justifyContent: "flex-end",
         alignItems: "center",
     },
     map: {
         height: "100%",
         width: "100%",
+        zIndex: 1
     },
-    drawerButton: {
-        zIndex: 100,
+    container2: {
+        position: "absolute",
+        height: 75,
+        width: 75,
+        zIndex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+
+    },
+    barMenu: {
+        paddingVertical: 8,
+        borderRadius: 6,
+        color: '#20232a',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: 7,
     }
+
 });
